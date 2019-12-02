5979ece78c36cf1db53ce80ef73e062d249b3c52
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2019-12-02 17:45:33 +0000

    readme added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000 
+const port = process.env.PORT || 4000
 
-app.use(express.static(`${__dirname}/dist`))
+app.use(express.static(`${__dirname}/src`))
 
-app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/src/index.html`))
 
 app.listen(port, () => console.log(`Express is running on port ${port}`))
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,3 @@
-// Refactoring notes
-// - added submit on 'enter'
-// - replaced 'Iam a londoner' function with Link 
-// - the code before refactoring is bellow the new code
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -29,7 +24,6 @@ class Home extends React.Component {
     this.props.history.push(`/weather/${this.state.city}`)
   }
 
-  //Added submission on Enter
   keyPress(e) {
     if (e.keyCode === 13) return this.handleSubmit()
   }
@@ -63,70 +57,3 @@ class Home extends React.Component {
 }
 
 export default Home
-
-
-
-
-// //BEFORE REFACTORING
-// import React from 'react'
-
-// import Greeting from './Greeting'
-
-
-// class Home extends React.Component {
-//   constructor() {
-//     super()
-
-//     this.state = {
-//       city: null
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//   }
-
-//   handleChange(e) {
-//     const city = e.target.value
-//     console.log(city)
-//     this.setState({ city })
-//   }
-
-//   handleSubmit(e) {
-//     e.preventDefault()
-//     if (!this.state.city) return null
-//     this.props.history.push(`/weather/${this.state.city}`)
-//   }
-
-//   handleSubmitLondon(e) {
-//     e.preventDefault()
-//     this.props.history.push('/weather/London')
-//   }
-
-//   componentDidMount(){
-//     console.log('mounted')
-//   }
-
-//   render() {
-
-//     return (
-//       <>
-//           <div className="container">
-//             <Greeting />
-//             <p className="subtitle is-2 has-text-centered">Where do you live?</p>
-//           </div>
-//           <section className="home">
-//             <input className="input" onChange={(e) => this.handleChange(e)}
-//               placeholder="Type in your city" 
-//             />
-//             <button className="button city" onClick={(e) => this.handleSubmit(e)} type="submit">LET'S GO!</button>
-//           </section>
-//           <div className="london">
-//             <button className="button london" onClick={(e) => this.handleSubmitLondon(e)} type="submit">HEY- I AM A LONDONER</button>
-//           </div>
-//           <div className="flying">
-//             <img className="catGif" src="http://giphygifs.s3.amazonaws.com/media/sIIhZliB2McAo/giphy.gif"></img>
-//           </div>
-//       </>
-//     )
-//   }
-// }
-
-// export default Home
